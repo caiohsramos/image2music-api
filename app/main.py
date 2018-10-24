f03a038337d9bed1b790af6ef3a3290d2dd3d2cd
Caio Ramos <caioramos97@gmail.com>
2018-10-24 19:59:41 -0300

    working spotify

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from flask import Flask
 import os
 from flask_cors import CORS
 from functools import wraps
-from flask import request, Response
+from flask import request, Response, jsonify
+import requests
 
 def check_auth(username, password):
     """This function is called to check if a username /
@@ -30,13 +31,28 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/watson')
-#@requires_auth
+@requires_auth
 def watson():
     return 'OK'
-@app.route('/api/spotify')
-#@requires_auth
+@app.route('/api/spotify', methods=['POST','OPTIONS'])
+@requires_auth
 def spotify():
-    return 'OK'
+    endpoint_token = 'https://accounts.spotify.com/api/token'
+    endpoint_api = 'https://api.spotify.com/v1/search'
+    headers_token = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    body_token = {
+        'grant_type': 'client_credentials',
+    }
+    auth = (os.environ['CLIENT_ID'], os.environ['CLIENT_SECRET'])
+
+    r = requests.post(endpoint_token, data=body_token, auth=auth, headers=headers_token)
+    r_final = requests.get(endpoint_api, 
+        headers={ 'Authorization': 'Bearer ' + r.json()['access_token'] },
+        params= { 'q': request.get_json()['query'], 'type': 'track' })
+    return jsonify(r_final.json())
+
 
 
 
